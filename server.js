f27b8eee9c2aba4fa17bba791da46e3539f54f64
ai-mac4 <vim>
2023-06-14 12:51:05 +0530

    fist cmmit

---
+++ server.js
@@ -0,0 +1,9 @@
+// console.log('Heeello');
+const express = require('express');
+const app=express()
+
+app.get('/',  (req, res)=> {
+    res.send('Hello World')
+  })
+
+app.listen(3000,()=>{console.log('node runnig pornt 30000000')})
